401b680959343f25518efba27686c346a7b3cd5c
xxiicode <leandrobilokapic@gmail.com>
2024-09-18 23:32:05 +0200

    Hola, primera vez abro un repo en VS community. intento de commit
    Añadir configuración inicial del proyecto y clase principal
    
    Se ha añadido un nuevo espacio de nombres `TP1_DSOO_CollecionClases` en el archivo `Program.cs`, y se ha creado una clase interna `Program` con un método `Main` que imprime "Hello, Grupo 14!" en la consola.
    
    Se ha creado un nuevo archivo de proyecto `TP1-DSOO-CollecionClases.csproj` con configuración para .NET 6.0, incluyendo propiedades como `OutputType`, `TargetFramework`, `RootNamespace`, `ImplicitUsings` y `Nullable`.
    
    Se han añadido varios archivos relacionados con la configuración de NuGet y MSBuild, incluyendo `project.assets.json`, `project.nuget.cache`, `TP1-DSOO-CollecionClases.csproj.nuget.dgspec.json`, `TP1-DSOO-CollecionClases.csproj.nuget.g.props`, `TP1-DSOO-CollecionClases.csproj.nuget.g.targets`, `.NETCoreApp,Version=v6.0.AssemblyAttributes.cs`, `TP1-DSOO-CollecionClases.AssemblyInfo.cs`, `TP1-DSOO-CollecionClases.AssemblyInfoInputs.cache`, `TP1-DSOO-CollecionClases.assets.cache`, `TP1-DSOO-CollecionClases.GeneratedMSBuildEditorConfig.editorconfig` y `TP1-DSOO-CollecionClases.GlobalUsings.g.cs`.

---
+++ TP1-DSOO-CollecionClases/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP1_DSOO_CollecionClases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Grupo 14!");
+        }
+    }
+}

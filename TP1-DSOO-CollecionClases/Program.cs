b9f9bb27ba801496c549a12299a6095492e6ec98
xxiicode <leandrobilokapic@gmail.com>
2024-09-18 23:40:57 +0200

    test 2 (borre algo mas, no figura)
    habia como una copia de proyecto, ahora deje una carpeta. dentro otra carpeta (mismo nombre) y dentro otra carpeta TP1-DSOO-ColleccionClases. nose

--- TP1-DSOO-CollecionClases/Program.cs
+++ TP1-DSOO-CollecionClases/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Grupo 14!!!");
+            Console.WriteLine("Hello, Grupo 14!!!!");
         }
     }
 }
